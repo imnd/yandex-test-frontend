cbaa70a8f905aee9f118d634a0c592856e24f6de
andreyserdyuk-sys <andrey.serdyuk@creativecode.digital>
2026-06-15 12:09:16 +0530

    bug fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -10,6 +10,21 @@ const api: AxiosInstance = axios.create({
     }
 });
 
+// Request interceptor to manually set X-XSRF-TOKEN header for cross-port/cross-origin requests
+api.interceptors.request.use(
+    (config) => {
+        const matches = document.cookie.match(/(?:^|; )XSRF-TOKEN=([^;]*)/);
+        const token = matches ? decodeURIComponent(matches[1]) : null;
+        if (token) {
+            config.headers['X-XSRF-TOKEN'] = token;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
 // Response interceptor to handle unauthenticated state automatically
 api.interceptors.response.use(
     (response: AxiosResponse) => response,
